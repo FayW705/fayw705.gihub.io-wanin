0d18db8f370fd62174dcb5a828957ab3409c4b53
FayW705 <38228674+FayW705@users.noreply.github.com>
2024-09-30 10:37:14 +0800

    Add files via upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,1 +1,51 @@
+<template>
+  <v-app>
+    
+    <AppBar @toggleDrawer="toggleDrawer" />
+    <NavigationDrawer v-if="$vuetify.breakpoint.mdAndUp" :drawer="drawer" :mini-variant.sync="miniVariant" />
+    
+    <v-main>
+      <v-container fluid>
+        <Carousels/>
+        <Played />
+        <Game/>
+      </v-container>
+      <Footer />
+    </v-main>
+    <BottomNav />
+  </v-app>
+</template>
 
+<script>
+import AppBar from '~/components/AppBar.vue'
+import NavigationDrawer from '~/components/NavigationDrawer.vue';
+import BottomNav from '~/components/BottomNav.vue'
+import Carousels from '~/components/Carousels.vue';
+import Played from '~/components/Played.vue'
+import Game from '~/components/Game.vue';
+import Footer from '~/components/footer.vue';
+
+export default {
+  components:{
+    AppBar,
+    NavigationDrawer,
+    BottomNav,
+    Carousels,
+    Played,
+    Game,
+    Footer
+  },
+  data() {
+    return{
+      drawer:true,
+      miniVariant:true
+    };
+  },
+  methods:{
+    toggleDrawer(){
+      this.miniVariant = !this.miniVariant;
+    }
+  }
+};
+
+</script>
